e77d8cff9c682f64899b8993c35b3e625a4d0340
mysupervisely <mysupervisely@gmail.com>
2026-09-25 11:05:51 -0400

    Allow secure native Church API requests

--- api/church.js
+++ api/church.js
@@ -9,9 +9,34 @@ const ALLOWED = [
   /^\/fasting\/\d{4}-\d{2}-\d{2}$/
 ];
 
+const CORS_ORIGINS = new Set([
+  'https://copticdailyprayer.app',
+  'capacitor://localhost',
+  'http://localhost'
+]);
+
+function applyCors(req, res) {
+  const origin = typeof req.headers.origin === 'string' ? req.headers.origin : '';
+  if (CORS_ORIGINS.has(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Vary', 'Origin');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Accept, Content-Type');
+    return true;
+  }
+  return !origin;
+}
+
 module.exports = async function handler(req, res) {
+  const corsAllowed = applyCors(req, res);
+  if (req.method === 'OPTIONS') {
+    return corsAllowed ? res.status(204).end() : res.status(403).end();
+  }
+  if (!corsAllowed) {
+    return res.status(403).json({error:'Origin not allowed'});
+  }
   if (req.method !== 'GET') {
-    res.setHeader('Allow', 'GET');
+    res.setHeader('Allow', 'GET, OPTIONS');
     return res.status(405).json({error:'Method not allowed'});
   }
 
