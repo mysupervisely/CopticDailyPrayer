6f95e24fb443ed24de5ed64d396d4749938127b4
mysupervisely <mysupervisely@gmail.com>
2026-09-25 08:20:01 -0400

    Configure Coptic Prayer native app identity

---
+++ capacitor.config.js
@@ -0,0 +1,10 @@
+const config = {
+  appId: 'org.copticdailyprayer.app',
+  appName: 'Coptic Prayer',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  ios: { contentInset: 'automatic' },
+  android: { allowMixedContent: false }
+};
+
+module.exports = config;
